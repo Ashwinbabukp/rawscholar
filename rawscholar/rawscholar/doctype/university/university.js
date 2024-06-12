32eec5ef34df822f690691413ef3cf398d283cb8
RameezRizvan <rameezrizvan04@gmail.com>
2024-06-12 10:58:58 +0530

    "feat: new doctypes"

---
+++ rawscholar/rawscholar/doctype/university/university.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Rawscholar dev team and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("University", {
+// 	refresh(frm) {
+
+// 	},
+// });
